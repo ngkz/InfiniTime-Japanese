60e43cfe470d2592c69a0cb1524f5e73f8bda84b
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-11-10 22:13:32 +0900

    nix build environment

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "virtual environments";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11"; # latest nixpkgs doesn't have gcc-arm-embedded-10
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      devshell,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+          config.allowUnfree = true; # nrf5-sdk
+        };
+      in
+      {
+        packages.default = pkgs.callPackage ./infinitime.nix { };
+        devShells.default = pkgs.devshell.mkShell {
+          devshell.packages = self.packages.${system}.default.nativeBuildInputs;
+
+          commands = [
+            {
+              name = "do_cmake";
+              command = ''
+                cmake -DARM_NONE_EABI_TOOLCHAIN_PATH=${pkgs.gcc-arm-embedded-10} -DNRF5_SDK_PATH=${pkgs.nrf5-sdk}/share/nRF5_SDK "$@"
+              '';
+            }
+          ];
+        };
+      }
+    );
+}
